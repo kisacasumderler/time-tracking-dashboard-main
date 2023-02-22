655edc2095ba44d7a545473abe658895a63a432f
Sümeyya Utku <89921699+kisacasumderler@users.noreply.github.com>
2023-02-22 16:43:31 +0300

    create time-tracking-dashboard-main

---
+++ App.js
@@ -0,0 +1,71 @@
+let DataId = document.querySelector("#DataId");
+
+let Data = (deger) => {
+    fetch('./data.json')
+        .then((response) => response.json())
+        .then((data) => DataFunc(data, deger));
+
+    let DataFunc = (data, deger) => {
+        data.forEach(element => {
+            RenderData(element, deger);
+        });
+    }
+}
+
+let Time;
+let liId ;
+
+let RenderData = (e, deger) => {
+
+    if (deger == "Daily") {
+        Time = `<b id='time'>${e.timeframes.daily.current}hrs</b><div>Last Daily - ${e.timeframes.daily.previous}hrs</div>`;
+    } else if (deger == "Monthly") {
+        Time = `<b id='time'>${e.timeframes.monthly.current}hrs</b><div>Last Monthly - ${e.timeframes.monthly.previous}hrs</div>`;
+    } else if (deger == "Weekly") {
+        Time = `<b id='time'>${e.timeframes.weekly.current}hrs</b><div>Last  Weekly- ${e.timeframes.weekly.previous}hrs</div>`;
+    }
+
+    if(e.title!="Self Care"){
+        liId = e.title;
+    }else {
+        liId = "Self";
+    }
+
+    let dataLi = `<li id='${liId}'><div class='contain'><h2><b>${e.title}</b><img src='./images/icon-ellipsis.svg' alt='${e.title}'></h2>${Time}</div></li>`;
+
+    DataId.innerHTML += dataLi;
+}
+
+
+Data("Daily");
+
+let buttons = document.querySelectorAll(".buttons button");
+buttons[0].style.color = "#fff";
+
+buttons[0].addEventListener("click", () => {
+    DataId.innerHTML = "";
+    Data("Daily");
+    styleClear();
+    buttons[0].style.color = "#fff";
+});
+
+buttons[1].addEventListener("click", () => {
+    DataId.innerHTML = "";
+    Data("Weekly");
+    styleClear();
+    buttons[1].style.color = "#fff";
+})
+
+buttons[2].addEventListener("click", () => {
+    DataId.innerHTML = "";
+    Data("Monthly");
+    styleClear();
+    buttons[2].style.color = "#fff";
+})
+
+
+let styleClear = () => {
+    buttons.forEach(button=>{
+        button.removeAttribute("style");
+    })
+}
---
+++ style-guide.md
@@ -0,0 +1,37 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+// Colors
+
+// Primary
+
+$Blue: hsl(246, 80%, 60%);
+
+$Lightred_work : hsl(15, 100%, 70%);
+$Softblue_play : hsl(195, 74%, 62%);
+$Lightred_study : hsl(348, 100%, 68%);
+$Limegreen_exercise : hsl(145, 58%, 55%);
+$Violet_social : hsl(264, 64%, 52%);
+$Softorange_selfCare : hsl(43, 84%, 65%);
+
+// Neutral
+
+$Verydarkblue: hsl(226, 43%, 10%);
+$Darkblue: hsl(235, 46%, 20%);
+$Desaturatedblue: hsl(235, 45%, 61%);
+$PaleBlue: hsl(236, 100%, 87%);
+
+// Typography / Body Copy
+
+$FontSize: 18px ; // (card titles e.g. Work, Play)
+
+### Font
+
+- Family: [Rubik](https://fonts.google.com/specimen/Rubik)
+- Weights: 300, 400, 500
